633d8c22d7a99369053b36ea85150dea7029444c
Chalkiope23 <meike.ahlers@web.de>
2018-05-17 20:43:46 +1200

    added gulp tasks for scss compilation and some scss styless

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
+'use strict';
+
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('sass', function () {
+ return gulp.src('app/scss/style.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('app/css'));
+});
 
-
-
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
 });
